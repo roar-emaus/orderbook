41227e980531f5d681e422b4532ea694ec226018
Roar Emaus <roar@xal.no>
2019-02-05 20:10:43 +0100

    blackified

--- src/read_orderbook.py
+++ src/read_orderbook.py
@@ -4,11 +4,11 @@ import numpy as np
 import pandas as pd
 import datetime as dt
 
-o = pd.read_csv('NEL_02_01.pd')
-o = o.set_index(pd.DatetimeIndex(o['time']))
+o = pd.read_csv("NEL_02_01.pd")
+o = o.set_index(pd.DatetimeIndex(o["time"]))
 
 
-columns = ['time', 'open','close', 'high', 'low', 'volume']
+columns = ["time", "open", "close", "high", "low", "volume"]
 freq = pd.Timedelta(minutes=30)
 g = o.groupby(pd.Grouper(freq=freq))
 candles = {col: [] for col in columns}
@@ -17,12 +17,12 @@ for name, group in g:
         open_v, close_v = group.price[-1], group.price[0]
         high_v, low_v = group.price.agg([np.max, np.min])
         vol_v = group.volume.sum()
-        candles['time'].append(name + freq//2)
-        candles['open'].append(open_v)
-        candles['close'].append(close_v)
-        candles['high'].append(high_v)
-        candles['low'].append(low_v)
-        candles['volume'].append(vol_v)
+        candles["time"].append(name + freq//2)
+        candles["open"].append(open_v)
+        candles["close"].append(close_v)
+        candles["high"].append(high_v)
+        candles["low"].append(low_v)
+        candles["volume"].append(vol_v)
 
 
 cf = pd.DataFrame(data=candles, columns=columns)
@@ -32,13 +32,40 @@ dec = cf.open > cf.close
 
 TOOLS = "pan,wheel_zoom,box_zoom,reset,save"
 
-candles = bp.figure(x_axis_type="datetime", tools=TOOLS, plot_height=750, plot_width=2000, title = "NEL 30 minutes")
-volume = bp.figure(x_range=candles.x_range, x_axis_type="datetime", tools=TOOLS, plot_height=250, plot_width=2000, title = "NEL volume")
-candles.grid.grid_line_alpha=0.3
+candles = bp.figure(
+    x_axis_type="datetime",
+    tools=TOOLS,
+    plot_height=750,
+    plot_width=2000,
+    title="NEL 30 minutes",
+)
+volume = bp.figure(
+    x_range=candles.x_range,
+    x_axis_type="datetime",
+    tools=TOOLS,
+    plot_height=250,
+    plot_width=2000,
+    title="NEL volume",
+)
+candles.grid.grid_line_alpha = 0.3
 candles.segment(cf.time, cf.high, cf.time, cf.low, color="black")
-candles.vbar(cf.time[inc], freq//2, cf.open[inc], cf.close[inc], fill_color="#D5E1DD", line_color="black")
-candles.vbar(cf.time[dec], freq//2, cf.open[dec], cf.close[dec], fill_color="#F2583E", line_color="black")
-volume.vbar(cf.time, freq//2, cf.volume*0, cf.volume, fill_color='blue')
+candles.vbar(
+    cf.time[inc],
+    freq // 2,
+    cf.open[inc],
+    cf.close[inc],
+    fill_color="#D5E1DD",
+    line_color="black",
+)
+candles.vbar(
+    cf.time[dec],
+    freq // 2,
+    cf.open[dec],
+    cf.close[dec],
+    fill_color="#F2583E",
+    line_color="black",
+)
+volume.vbar(cf.time, freq//2, cf.volume*0, cf.volume, fill_color="blue")
 layout = bl.layout(candles, volume)
 
 bp.output_file("NEL.html", title="NEL 30 minutes")
--- src/stock_collector.py
+++ src/stock_collector.py
@@ -6,59 +6,80 @@ import datetime as dt
 
 
 def get_orderbook(identifier, market):
-    url = ('https://www.nordnet.no/mux/popups/marknaden/'
-                'aktiehemsidan/avslut.html?containerid=avslut&'
-                f'identifier={identifier}&marketplace={market}&limit=1000000')
+    url = (
+        "https://www.nordnet.no/mux/popups/marknaden/"
+        "aktiehemsidan/avslut.html?containerid=avslut&"
+        f"identifier={identifier}&marketplace={market}&limit=1000000"
+    )
     today_date = dt.date.today()
     r = requests.get(url)
-    columns = ['time', 'volume', 'price', 'buyer', 'seller']
+    columns = ["time", "volume", "price", "buyer", "seller"]
     orderbook = {col: [] for col in columns}
-    ticker = r.text.split('</h1>')[0].split('</span>')[-1].strip().replace(' ','_')
-    print(f'Retrieving orderbook for {ticker}')
+    ticker = r.text.split("</h1>")[0].split("</span>")[-1].strip().replace(" ", "_")
+    print(f"Retrieving orderbook for {ticker}")
     tr_elements = [line for line in r.text.split("</tr>")][1:-1]
     for element in tr_elements:
-        td_elements = [td for td in element.split('<td')]
-        buyer = td_elements[1].split("title=")[1].split(">")[0].replace('"','').strip()
-        seller = td_elements[2].split("title=")[1].split(">")[0].replace('"','').strip()
-        volume = int(td_elements[3].replace(">","").replace("</td","").replace(" ",""))
-        price = float(td_elements[4].split('</span>')[0].split('>')[-1].replace(",",".").replace(' ', ''))
-        time = dt.datetime.strptime(td_elements[5].split('"last">')[1].split('<img')[0].strip(),
-                '%H:%M:%S').time()
-        timestamp = dt.datetime.combine(today_date, time)#.timestamp()
-        orderbook['time'].append(timestamp)
-        orderbook['volume'].append(volume)
-        orderbook['price'].append(price)
-        orderbook['buyer'].append(buyer)
-        orderbook['seller'].append(seller)
+        td_elements = [td for td in element.split("<td")]
+        buyer = td_elements[1].split("title=")[1].split(">")[0].replace('"', "").strip()
+        seller = (
+            td_elements[2].split("title=")[1].split(">")[0].replace('"', "").strip()
+        )
+        volume = int(
+            td_elements[3].replace(">", "").replace("</td", "").replace(" ", "")
+        )
+        price = float(
+            td_elements[4]
+            .split("</span>")[0]
+            .split(">")[-1]
+            .replace(",", ".")
+            .replace(" ", "")
+        )
+        time = dt.datetime.strptime(
+            td_elements[5].split('"last">')[1].split("<img")[0].strip(), "%H:%M:%S"
+        ).time()
+        timestamp = dt.datetime.combine(today_date, time)  # .timestamp()
+        orderbook["time"].append(timestamp)
+        orderbook["volume"].append(volume)
+        orderbook["price"].append(price)
+        orderbook["buyer"].append(buyer)
+        orderbook["seller"].append(seller)
 
     orderbook_frame = pd.DataFrame(orderbook, columns=columns)
-    orderbook_frame.to_csv(f'../data/{ticker}_{market}_{today_date.strftime("%m_%d")}.pd',
-                           sep=',', encoding='utf-8', index=False)
+    orderbook_frame.to_csv(
+        f'../data/{ticker}_{market}_{today_date.strftime("%m_%d")}.pd',
+        sep=",",
+        encoding="utf-8",
+        index=False,
+    )
 
 
 def get_stocks(url):
     r = requests.get(url)
-    soup = bs4.BeautifulSoup(r.content, 'lxml')
-    hits = soup.find_all('a', class_=('underline'))
+    soup = bs4.BeautifulSoup(r.content, "lxml")
+    hits = soup.find_all("a", class_=("underline"))
     stocks = dict()
     for hit in hits:
-        name = ''.join(s for s in hit.text.replace(' ', 'abcde') if s.isalnum())
-        name = name.replace('abcde', '_')
-        id_and_m = [k.split('=')[1] for k in hit['href'].split('?')[1].split('&')]
+        name = "".join(s for s in hit.text.replace(" ", "abcde") if s.isalnum())
+        name = name.replace("abcde", "_")
+        id_and_m = [k.split("=")[1] for k in hit["href"].split("?")[1].split("&")]
         stocks[name] = id_and_m
     return stocks
 
 
-if __name__=='__main__':
-    market_names = ['Norge', 'Sverige', 'Danmark']
+if __name__ == "__main__":
+    market_names = ["Norge", "Sverige", "Danmark"]
     markets = []
     for m in market_names:
         time.sleep(3)
-        markets.append(get_stocks('https://www.nordnet.no/mux/web/marknaden/kurslista/'
-                                f'aktier.html?marknad={m}&lista=1_1&large=on&mid=on&'
-                                 'small=on&sektor=0&subtyp=price&sortera=aktie&'
-                                 'sorteringsordning=stigande'))
-    
+        markets.append(
+            get_stocks(
+                "https://www.nordnet.no/mux/web/marknaden/kurslista/"
+                f"aktier.html?marknad={m}&lista=1_1&large=on&mid=on&"
+                "small=on&sektor=0&subtyp=price&sortera=aktie&"
+                "sorteringsordning=stigande"
+            )
+        )
+
     for market in markets:
         for stock in market.values():
             time.sleep(2)
